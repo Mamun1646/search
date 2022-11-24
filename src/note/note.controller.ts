8438d6374cdead9819512e59fc7aedab5df71431
Mamun1646 <mimbd0113000gmail.com>
2022-11-24 17:10:55 +0600

    vercel config

--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { NoteService } from './note.service';
 import { CreateNoteInput } from './dto/create-note.input';
 
-@Controller()
+@Controller('note')
 export class NoteController {
   constructor(private readonly noteService: NoteService) {}
 
