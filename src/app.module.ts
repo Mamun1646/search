ac83138bee17db9b75c6707738ffd7961fa513b9
Mamun1646 <mimbd0113000gmail.com>
2022-11-24 18:09:36 +0600

    without appolo server plug in

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+// import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QuranModule } from './quran/quran.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
@@ -14,9 +14,9 @@ import { AppController } from './app.controller';
   GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       debug: false,
-      playground: false,
+      playground: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      // plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     MongooseModule.forRoot('mongodb+srv://quran:1234@cluster0.kemb19w.mongodb.net/quran-sharif'),
     QuranModule,
