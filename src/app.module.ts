d21dd808ae001afb81becb770982ce39f7ce41a1
Mamun1646 <mimbd0113000gmail.com>
2022-11-25 10:17:30 +0600

    config Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,24 +5,27 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { QuranModule } from './quran/quran.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 import { NoteModule } from './note/note.module';
 import { AppController } from './app.controller';
 @Module({
   imports: [
-
-
-  GraphQLModule.forRoot<ApolloDriverConfig>({
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.local.env',
+      //envFilePath:".prod.env"
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       debug: false,
       playground: false,
-       cache: 'bounded',
+      cache: 'bounded',
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
-    MongooseModule.forRoot('mongodb+srv://quran:1234@cluster0.kemb19w.mongodb.net/quran-sharif'),
+    MongooseModule.forRoot(process.env.MONGO_URL),
     QuranModule,
     NoteModule,
-  
   ],
   controllers: [AppController],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3005);
+  // console.log(process.env.port)
+  await app.listen(process.env.port);
 }
 bootstrap();
